9efe81f957afb57a8e71d34d3e5c95b445ff944a
Nico <ramicaza@gmail.com>
2015-03-26 20:45:08 +0700

    New and Improved ArdeeSoul

--- ArdeeSoul/ArdeeSoul.ino
+++ ArdeeSoul/ArdeeSoul.ino
@@ -2,34 +2,35 @@
 
 #define dataWidth 32
 #define motorNumber 8
-
+int motorMap[8] = {8, 4, 9, 6, 5, 7, 2, 3}; //map to figure out which pin goes to which motor
+bool reverseMap[8] = {true, false, true, false, false, false, true, true}; //map to find which motors to revers
 Servo mots[motorNumber];
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial1.begin(9600);
-  for (int i = 2; i < motorNumber; i++) {
-    mots[i].attach(i); //attach motor i in array to to pin i
+  for (int i = 0; i < motorNumber; i++) {
+    mots[i].attach(motorMap[i]); //attach motor i in array to to pin i
+    mots[i].write(90);
   }
-  pinMode(13,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available()>=dataWidth+4){
-    byte in[dataWidth+4];
-    for(int i=0;i<dataWidth+4;i++){
+  if (Serial.available() >= dataWidth + 4) {
+    byte in[dataWidth + 4];
+    for (int i = 0; i < dataWidth + 4; i++) {
       in[i] = Serial.read();
     }
     delay(5);//wait for any accidental extra data leaving ~15ms to next BC
-    while(Serial.available()>0){
+    while (Serial.available() > 0) {
       Serial.read();//read away remaining data
     }
 
     //respond to raspi with 32 data and a \n
-    for(int i=0;i<dataWidth;i++){
-      Serial.write(random(65,90));
+    for (int i = 0; i < dataWidth; i++) {
+      Serial.write(random(65, 90));
     }
     Serial.write('8');
     Serial.write('=');
@@ -45,26 +46,23 @@ void loop() {
   }
 }
 
-void parse(char bytes[]){
-  //using the channel incrementor write to the rest of the channels here 
+void parse(char bytes[]) {
+  //using the channel incrementor write to the rest of the channels here
   //i.e. if(bytes[8]==127) then set lights to high
   int i = 0; //i is the channel incrementor and is not to be fucked with
-  for(i=0;i<motorNumber;i++){
-    int motAsServo = map(bytes[i], -127, 127, 0, 180);//map receiver bytes
-    mots[i+2].write(motAsServo); //set servos on pins 2-9 to correct vals.
+  for (i = 0; i < motorNumber; i++) {
+    setMotor(i, bytes[i]);
   }
-  for(int i=0;i<6;i++){
-    //Serial1.print(bytes[i],DEC);
-  }
-  //Serial1.println(bytes[0],DEC);
-  if(bytes[0]>50){
-    digitalWrite(13,HIGH);
-  }
-  else{
-    digitalWrite(13,LOW);
-  }
+  //setMotor(7,bytes[1]);//to test if they pull too much I alone 
 }
-//0bcdefghijklmnopqrstuvwxyzabcdef8=D 
+void setMotor(int motorIndex, int value) {
+  value = (reverseMap[motorIndex] == true) ? -value : value;
+  int motAsServo = map(value, -381, 381, 0, 175);//map receiver bytes should by -127 to 127 but this too fast
+  mots[motorIndex].write(motAsServo); //set servos on pins 2-9 to correct vals.
+  //Serial.print("MOT: ");
+  //Serial.println(motAsServo, DEC);
+}
+//0bcdefghijklmnopqrstuvwxyzabcdef8=D
 
 
 
